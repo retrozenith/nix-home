65b8634610a77c9c55c6be5ab02bd4ba014f16b3
Cristea Florian Victor <80767544+retrozenith@users.noreply.github.com>
2026-01-22 20:17:40 +0200

    chore(h610): add dev and printing packages

--- modules/h610/packages.nix
+++ modules/h610/packages.nix
@@ -19,11 +19,59 @@
     # Python
     python3
     python3Packages.pip
+
+    # Node
+    bun
+    nodejs_22
+
+    # Android
+    android-studio
+    android-tools
+
+    # Java
+    openjdk21
+
+    # Make
+    gnumake
+    cmake
+
+    # C++
+    gcc
+    gdb
+
+    # Go
+    go
+
+    # Canon Printer
+    cnijfilter2
+    simple-scan
+    xsane
+    gscan2pdf
+    sane-backends
+    sane-airscan
   ];
+
+  # Accept Android SDK license
+  nixpkgs.config.android_sdk.accept_license = true;
 
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
     pinentryPackage = pkgs.pinentry-qt;
   };
+
+  services.printing = {
+    enable = true;
+    drivers = [ pkgs.cnijfilter2 ];
+  };
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+
+  hardware.sane.enable = true;
+  services.udev.packages = [ pkgs.sane-airscan ];
+
 }
